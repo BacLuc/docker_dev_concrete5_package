73dbdd6a6141fed6abf657770894e700e74b21e5
BacLuc <lucius.bachmann@gmx.ch>
2019-03-21 00:13:21 +0100

    docker: seperate db and webserver

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,28 @@
-FROM fauria/lamp
+FROM php:7.1-apache
 MAINTAINER Lucius Bachmann <lucius.bachmann@gmx.ch>
-LABEL Description="Docker Container to run newGryfiPage" \
+LABEL Description="Docker Container to develop concrete5 projects" \
 	License="Apache License 2.0" \
-	Usage="docker run -d -p [HOST WWW PORT NUMBER]:80 -p [HOST DB PORT NUMBER]:3306 -v [HOST WWW DOCUMENT ROOT]:/var/www/html bacluc/newgryfipage" \
-Version="1.0"
+	Usage="docker compose up" \
+    Version="1.0"
 
-
+RUN rm /etc/apt/preferences.d/no-debian-php
 RUN apt-get update
-RUN apt-get install -y php-zip php-mbstring dos2unix php-xdebug
+RUN apt-get install -y dos2unix mariadb-client iproute2 gettext-base
+RUN docker-php-ext-install pdo_mysql
+RUN pecl install xdebug
+
+RUN cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
 
 COPY docker/entrypoint.sh /usr/local/bin
-
 RUN dos2unix /usr/local/bin/entrypoint.sh
 
+ENV CONCRETE_SUPPORT_FILES=/usr/local/etc/concrete5/
+RUN mkdir -p $CONCRETE_SUPPORT_FILES
+COPY docker/*.sql $CONCRETE_SUPPORT_FILES
+COPY docker/database.php.template $CONCRETE_SUPPORT_FILES
+
 VOLUME /var/www/html
-VOLUME /var/log/httpd
-VOLUME /var/lib/mysql
-VOLUME /var/log/mysql
+VOLUME /var/log/apache2
 
 EXPOSE 80
 EXPOSE 3306
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,79 +1,26 @@
 #!/bin/bash
-
-function exportBoolean {
-    if [ "${!1}" = "**Boolean**" ]; then
-            export ${1}=''
-    else
-            export ${1}='Yes.'
-    fi
-}
-
-exportBoolean LOG_STDOUT
-exportBoolean LOG_STDERR
-
-if [ $LOG_STDERR ]; then
-    /bin/ln -sf /dev/stderr /var/log/apache2/error.log
-else
-	LOG_STDERR='No.'
-fi
-
-if [ $ALLOW_OVERRIDE == 'All' ]; then
-    /bin/sed -i 's/AllowOverride\ None/AllowOverride\ All/g' /etc/apache2/apache2.conf
-fi
-
-if [ $LOG_LEVEL != 'warn' ]; then
-    /bin/sed -i "s/LogLevel\ warn/LogLevel\ ${LOG_LEVEL}/g" /etc/apache2/apache2.conf
-fi
-
-# enable php short tags:
-/bin/sed -i "s/short_open_tag\ \=\ Off/short_open_tag\ \=\ On/g" /etc/php/7.0/apache2/php.ini
-
-# stdout server info:
-if [ ! $LOG_STDOUT ]; then
-cat << EOB
-
-    **********************************************
-    *                                            *
-    *    Docker image: fauria/lamp               *
-    *    https://github.com/fauria/docker-lamp   *
-    *                                            *
-    **********************************************
-    SERVER SETTINGS
-    ---------------
-    · Redirect Apache access_log to STDOUT [LOG_STDOUT]: No.
-    · Redirect Apache error_log to STDERR [LOG_STDERR]: $LOG_STDERR
-    · Log Level [LOG_LEVEL]: $LOG_LEVEL
-    · Allow override [ALLOW_OVERRIDE]: $ALLOW_OVERRIDE
-    · PHP date timezone [DATE_TIMEZONE]: $DATE_TIMEZONE
-EOB
-else
-    /bin/ln -sf /dev/stdout /var/log/apache2/access.log
-fi
-
 # Set PHP timezone
-/bin/sed -i "s/\;date\.timezone\ \=/date\.timezone\ \=\ ${DATE_TIMEZONE}/" /etc/php/7.0/apache2/php.ini
+#/bin/sed -i "s/\;date\.timezone\ \=/date\.timezone\ \=\ ${DATE_TIMEZONE}/" /etc/php/7.0/apache2/php.ini
 
 # Run Postfix
 /usr/sbin/postfix start
 
-
-
-
-
-
-#part of bacluc
-# Run MariaDB
-service mysql start
+XDEBUG_FILE=/usr/local/etc/php/conf.d/20-xdebug.ini
+GATEWAY=$(ip route show dev eth0 | awk '/default/ { print $3 }')
+echo "zend_extension=xdebug.so" > $XDEBUG_FILE
+echo "xdebug.remote_enable=on" >> $XDEBUG_FILE
+echo "xdebug.remote_autostart=off" >> $XDEBUG_FILE
+echo "xdebug.remote_host=$GATEWAY" >> $XDEBUG_FILE
 
 CONCRETE5_DIR=/var/www/html
-DATABASE_NAME=concrete5_7
 
-mysql --execute="create database $DATABASE_NAME collate utf8mb4_bin;"
-
-mv $CONCRETE5_DIR/application/config/database.php $CONCRETE5_DIR/application/config/database.backup.php
-cp $CONCRETE5_DIR/docker/database.php $CONCRETE5_DIR/application/config/database.php
+mkdir -p $CONCRETE5_DIR/application/config
+envsubst < /usr/local/etc/concrete5/database.php.template > $CONCRETE5_DIR/application/config/database.php
 
 chmod +x $CONCRETE5_DIR/concrete/bin/concrete5
+
+chmod -R 755 $CONCRETE5_DIR/application/files/
+chown -R www-data $CONCRETE5_DIR/application/files/
 
 #because installing concrete5 is too slow, we use a database backup
 #where the theme is installed and set
@@ -88,35 +35,26 @@ chmod +x $CONCRETE5_DIR/concrete/bin/concrete5
 #    --admin-email=someemail@domain.ch \
 #    --admin-password=password \
 #    --default-locale=en_US
+MYSQL_OPTIONS="-h db -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE"
 
+until mysql $MYSQL_OPTIONS --execute 'exit'; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
 
-mysql $DATABASE_NAME < /var/www/html/docker/concrete5_7_with_theme.sql
+>&2 echo "MySQL is up - executing command"
+
+mysql $MYSQL_OPTIONS < $CONCRETE_SUPPORT_FILES/concrete5_7_with_theme.sql
 
 
 $CONCRETE5_DIR/concrete/bin/concrete5 c5:package-install basic_table_package
 
 chmod -R +x $CONCRETE5_DIR/application/files
 
-mysql $DATABASE_NAME < /var/www/html/docker/concrete5_7_with_basictable.sql
-
-mysql --execute "update mysql.user set password=PASSWORD('root'), plugin = NULL;"
-
-service mysql restart
-###
-####setup xdebug
-PHP_VERSION_FOLDER=$(php -r "echo phpversion();" | egrep -o -m 1 "[0-9]\.[0-9]" | head -n 1)
-XDEBUG_FILE=/etc/php/$PHP_VERSION_FOLDER/apache2/conf.d/20-xdebug.ini
-echo "zend_extension=xdebug.so" > $XDEBUG_FILE
-echo "xdebug.remote_enable=on" >> $XDEBUG_FILE
-echo "xdebug.remote_autostart=off" >> $XDEBUG_FILE
-echo "xdebug.remote_host=$XDEBUG_CONFIG" >> $XDEBUG_FILE
-
-
-
+mysql $MYSQL_OPTIONS < $CONCRETE_SUPPORT_FILES/concrete5_7_with_basictable.sql
 
 service apache2 restart
 tail -f /dev/null
-
 
 #to clear the cache when needed
 #$CONCRETE5_DIR/concrete/bin/concrete5 c5:clear-cache
